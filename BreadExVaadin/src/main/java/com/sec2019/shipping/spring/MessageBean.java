c9c24463eb940ce045d95e7a62e16594c96f0ba0
evan-krul <evan@krul.ca>
2019-10-26 09:25:18 -0600

    Initial vaadin instance

---
+++ BreadExVaadin/src/main/java/com/sec2019/shipping/spring/MessageBean.java
@@ -0,0 +1,14 @@
+package com.sec2019.shipping.spring;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageBean implements Serializable {
+
+    public String getMessage() {
+        return "Button was clicked at " + LocalTime.now();
+    }
+}
